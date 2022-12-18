efa08be5b20e34012daeedab2dc093dd51535c4a
Jena <116511683+NabiHoney@users.noreply.github.com>
2022-12-18 12:25:48 -0600

    Create Ch2Ex9.py

---
+++ Ch2Ex9.py
@@ -0,0 +1,10 @@
+# prints hello if 1, howdy if 2, greeting if else.
+
+print("Please enter a number.")
+spam = input()
+if spam == '1':
+    print('Hello')
+elif spam == '2':
+    print('Howdy')
+else:
+    print('Greetings')
